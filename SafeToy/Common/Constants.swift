c55fa77cb2deaafd7c053b6c97cbd80b506bddb9
Jonathan <jonathan@Jonathans-iMac.fios-router.home>
2017-04-13 00:04:09 -0400

    WIP camera viewcontrollers

--- SafeToy/Common/Constants.swift
+++ SafeToy/Common/Constants.swift
@@ -7,6 +7,9 @@
 //
 
 import Foundation
+import UIKit
 
 let awsAccessKey = "AKIAIGOX2DGD7LBX7I7Q"
 let awsSecretKey = "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"
+let largeFont = UIScreen.main.bounds.width * 0.03
+let mainFont = UIScreen.main.bounds.width * 0.01
--- SafeToy/Controllers/AddImageViewController.swift
+++ SafeToy/Controllers/AddImageViewController.swift
@@ -13,33 +13,21 @@ class AddToyViewController: UIViewController,UIImagePickerControllerDelegate,
 UINavigationControllerDelegate {
     
     var previewView = UIView()
-    var recordBtn = FlatButton()
+    var cameraBtn = FlatButton()
+    var albumBtn = FlatButton()
     
     let picker = UIImagePickerController()
    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.addSubview(previewView)
-        self.view.addSubview(recordBtn)
+        self.view.addSubview(cameraBtn)
+        self.view.addSubview(albumBtn)
+        
         self.navigationController?.isNavigationBarHidden = true
         
         picker.delegate = self
-        
-        picker.allowsEditing = false
-        picker.sourceType = .photoLibrary
-        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
-        present(picker, animated: true, completion: nil)
-        
-        
-        //camera
-        /*
-         picker.allowsEditing = false
-         picker.sourceType = UIImagePickerControllerSourceType.camera
-         picker.cameraCaptureMode = .photo
-         picker.modalPresentationStyle = .fullScreen
-         present(picker,animated: true,completion: nil)*/
 
         // Do any additional setup after loading the view.
     }
@@ -51,7 +39,39 @@ UINavigationControllerDelegate {
     
     func setup(){
         
-        recordBtn.backgroundColor = Color.grey.lighten5
+        previewView.backgroundColor = Color.black
+        
+        albumBtn.backgroundColor = Color.grey.lighten5
+        albumBtn.setTitle("Album", for: .normal)
+        albumBtn.titleColor = Color.grey.darken4
+        albumBtn.titleLabel?.font = RobotoFont.bold(with: largeFont)
+        albumBtn.addTarget(self, action: #selector(AddToyViewController.fromAlbum), for: .touchUpInside)
+        
+        cameraBtn.backgroundColor = UIColor.clear
+        cameraBtn.borderColor = Color.grey.lighten5
+        cameraBtn.borderWidth = 3
+        cameraBtn.setTitle("Camera", for: .normal)
+        cameraBtn.titleColor = Color.grey.lighten5
+        cameraBtn.titleLabel?.font = RobotoFont.bold(with: largeFont)
+        cameraBtn.addTarget(self, action: #selector(AddToyViewController.fromCamera), for: .touchUpInside)
+        
+    }
+    
+    func fromAlbum(){
+        
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func fromCamera(){
+        
+        picker.allowsEditing = false
+        picker.sourceType = UIImagePickerControllerSourceType.camera
+        picker.cameraCaptureMode = .photo
+        picker.modalPresentationStyle = .fullScreen
+        present(picker,animated: true,completion: nil)
     }
     
     func constrainViews(){
@@ -62,11 +82,17 @@ UINavigationControllerDelegate {
         previewView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0).isActive = true
         previewView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0).isActive = true
         
-        recordBtn.translatesAutoresizingMaskIntoConstraints = false
-        recordBtn.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -50).isActive = true
-        recordBtn.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
-        recordBtn.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.2).isActive = true
-        recordBtn.heightAnchor.constraint(equalTo: recordBtn.widthAnchor, multiplier: 1).isActive = true
+        cameraBtn.translatesAutoresizingMaskIntoConstraints = false
+        cameraBtn.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -50).isActive = true
+        cameraBtn.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 20).isActive = true
+        cameraBtn.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.2).isActive = true
+        cameraBtn.heightAnchor.constraint(equalTo: cameraBtn.widthAnchor, multiplier: 1).isActive = true
+        
+        albumBtn.translatesAutoresizingMaskIntoConstraints = false
+        albumBtn.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -50).isActive = true
+        albumBtn.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -20).isActive = true
+        albumBtn.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.2).isActive = true
+        albumBtn.heightAnchor.constraint(equalTo: albumBtn.widthAnchor, multiplier: 1).isActive = true
     }
     
     //MARK: - Delegates
